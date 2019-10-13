1909a7d950bba0292e03650cf3bb85f34ee861d8
Atif Mehmood <35393156+am02464@users.noreply.github.com>
2019-10-13 13:59:02 +0500

    Updates

---
+++ precache-manifest.4fc5bde655e3656c078502ea609e7537.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1c054c6e4c502df0e6b02062c6ae386",
+    "url": "/simple-csv-reader/index.html"
+  },
+  {
+    "revision": "8e9b2df4c26ae8846f83",
+    "url": "/simple-csv-reader/static/css/main.ef762d57.chunk.css"
+  },
+  {
+    "revision": "bb80b0ea0e3a6da3c9fe",
+    "url": "/simple-csv-reader/static/js/2.a018cfa8.chunk.js"
+  },
+  {
+    "revision": "8e9b2df4c26ae8846f83",
+    "url": "/simple-csv-reader/static/js/main.aa564298.chunk.js"
+  },
+  {
+    "revision": "9f2369c70d449a81ff63",
+    "url": "/simple-csv-reader/static/js/runtime-main.e78914bf.js"
+  }
+]);
